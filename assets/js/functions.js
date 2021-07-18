becde3b00b6ef3e0e7c20c9c76d54352d8958107
Shahahdat <raselsha@gmail.com>
2021-07-18 16:16:47 +0600

    icon and id array updated

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,14 +1,55 @@
-var words=['word1','word2','word3','word4','word5','word6','word7','word8','word9','word10'];
-var mic=['mic1','mic2','mic3','mic4','mic5','mic6','mic7','mic8','mic9','mic10'];
-var sound=['sound1','sound2','sound3','sound4','sound5','sound6','sound7','sound8','sound9','sound10'];
-var audio=['audio1','audio2','audio3','audio4','audio5','audio6','audio7','audio8','audio9','audio10'];
+var words=['w1','w2','w3','w4','w5','w6','w7','w8','w9','w10'];
+var mic=['m1','m2','m3','m4','m5','m6','m7','m8','m9','m10'];
+var sound=['s1','s2','s3','s4','s5','s6','s7','s8','s9','s10'];
+var audio=['a1','a2','a3','a4','a5','a6','a7','a8','a9','a10'];
 
 var i=0;
 reset();
 start();
 
-function main(id) {
-	userSound();
+function reset(){
+	for (var i = 0; i < 10; i++) {
+	  	inactive(mic[i]);
+	  	inactive(sound[i]);
+	  	hide(audio[i]);
+	}
+}
+
+function start() {
+	if(i==0){
+	  wordActive(words[i]);
+	  addRecordingBtn(mic[i]);
+	  active(mic[i]);
+	}
+}
+
+function recordStart(id) {
+	var element = document.getElementById(id);
+	element.src = 'assets/img/stop.svg';
+  	startRecording();
+  	inactive(sound[i]);
+}
+
+function recordStop(id) {
+	var element = document.getElementById(id);
+	element.src = 'assets/img/mic.svg';
+	stopRecording();
+}
+
+function startRecording(){
+	console.log('start Recording');
+}
+
+function stopRecording(){
+	console.log('stop Recording');
+	storeRecording();;
+}
+function storeRecording(){
+	console.log('store Recording');
+	
+	setTimeout(function(){
+	    userSound();
+	},0);
 }
 
 function userSound(){
@@ -26,10 +67,12 @@ function addPlaySound(id){
 function userPlaySound(id) {
 	playSound(id)
 	removeUserPlaySound(id);
-	removeMainFunc(mic[i]);
+	removeRecordingBtn(mic[i]);
+	wordInactive(words[i]);
 	inactive(mic[i]);
 	nextWord();
 }
+
 function removeUserPlaySound(id){
 	var sound = document.getElementById(id);
 	var att = document.createAttribute("onclick");
@@ -43,12 +86,28 @@ function playSound(id){
 	audio.play();	
 }
 
+function addRecordingBtn(id){
+	var element = document.getElementById(id);
+	var mousedown = document.createAttribute("onmousedown");
+	var mouseup = document.createAttribute("onmouseup");
+	mousedown.value = "recordStart('"+id+"')";
+	element.setAttributeNode(mousedown);
+
+	mouseup.value = "recordStop('"+id+"')";
+	element.setAttributeNode(mouseup);
+}
+
+function removeRecordingBtn(id){
+	var element = document.getElementById(id);
+	element.removeAttribute('onmouseup');
+	element.removeAttribute('onmousedown');
+}
+
 function nextWord(){
-	makeInactive(words[i]);
 	i++;
 	if (i<10) {
-		makeActive(words[i]);
-		addMainFunc(mic[i]);
+		wordActive(words[i]);
+		addRecordingBtn(mic[i]);
 		active(mic[i]);
 	}
 	else{
@@ -63,25 +122,12 @@ function playOriginalAudio(id) {
 		audio.play();
 }
 
-function addMainFunc(id) {
-	var mic = document.getElementById(id);
-	var att = document.createAttribute("onclick");
-	att.value = "main('"+id+"')";
-	mic.setAttributeNode(att);
-}
-
-function removeMainFunc(id){
-	var mic = document.getElementById(id);
-	mic.removeAttribute('onclick');
-}
-
-
-function makeActive(id){
+function wordActive(id){
 	var h1 = document.getElementById(id);
 	h1.classList.add('text-green');
 }
 
-function makeInactive(id){
+function wordInactive(id){
 	var h1 = document.getElementById(id);
 	h1.classList.remove('text-green');
 	h1.classList.add('text-white');
@@ -107,13 +153,6 @@ function inactive(id){
 	item.classList.remove('pointer');
 }
 
-function start() {
-	if(i==0){
-	  makeActive(words[i]);
-	  addMainFunc(mic[i]);
-	  active(mic[i]);
-	}
-}
 
 function originalAudio(){
 	for (var i = 0; i < audio.length; i++) {
@@ -127,13 +166,7 @@ function endLesson() {
 	help.classList.add('d-none');
 	subtitle.classList.remove('d-none');
 }
-function reset(){
-	for (var i = 0; i < audio.length; i++) {
-	  	inactive(mic[i]);
-	  	inactive(sound[i]);
-	  	hide(audio[i]);
-	}
-}
+
 
 
 
